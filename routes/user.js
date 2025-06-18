dce671c7c050f88879fbaa7f4afe9bd9b5a14189
HamNP <ntwpjtr@gmail.com>
2025-06-18 12:46:39 +0700

    init

---
+++ routes/user.js
@@ -0,0 +1,48 @@
+import express from "express";
+//Controllers
+import { listUser,readUser,createUser,updateRoleUser,deleteUser } from "../controllers/user.js";
+
+const router = express.Router()
+
+
+
+// router.get('/users',(req,res)=>{
+//   //code body
+// res.json({message:"This is GET Users"})
+// });
+
+router.get('/users',listUser);
+
+// router.get("/user",(req,res)=>{
+//   //code body
+//   res.json({message:"This is GET User"})
+// })
+router.get("/user",readUser);
+
+
+// router.post('/user',(req,res)=>{
+//   //code body
+//   res.json({message:"This is POST user"})
+// })
+router.post('/user',createUser)
+
+// router.patch('/user/role/:id',(req,res)=>{
+//   //code cody
+//   // console.log(req.params.id)
+//   const {id} = req.params;
+//   console.log(id)
+//   res.json({message:"This is PATCH role"})
+// })
+
+router.patch('/user/role/:id',updateRoleUser)
+
+// router.delete("/user/:id",(req,res)=>{
+//   const {id} = req.params;
+//   res.json({message:`This is Delete ${id}`})
+// })
+
+router.delete("/user/:id",deleteUser)
+
+// Export
+
+export default router
---
+++ server.js
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import cors from "cors";
+import morgan from "morgan";
+//Routing
+import userRouter from "./routes/user.js"
+import authRouter from "./routes/auth.js"
+
+const app = express()
+
+//Basic middlewares
+app.use(cors()); // Allow cross domains
+app.use(morgan('dev')); // Show logs
+app.use(express.json()); // For read body
+
+// Routing GET,POST,PUT,PATCH,DELETE
+// http://localhost:8000
+// app.get('/',(req,res)=>{
+//   // Code body
+//   res.json({message:"Hello CC20"})
+// })
+
+//ENDPOINT http://localhost:8000/api/users
+app.use('/api',userRouter)
+app.use('/auth',authRouter)
+
+
+const PORT = 8000
+//Start Server
+app.listen(8000,()=>console.log(`Server is running on port ${PORT}`))
+
